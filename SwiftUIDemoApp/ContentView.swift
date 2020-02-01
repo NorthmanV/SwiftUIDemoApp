8ef38df39760ec0a053442c04332890c7928b7c4
Ruslan Akberov <ruslanakberov@gmail.com>
2020-02-01 17:14:27 +0300

    Initial Commit

---
+++ SwiftUIDemoApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIDemoApp
+//
+//  Created by Ruslan Akberov on 01.02.2020.
+//  Copyright © 2020 Ruslan Akberov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
